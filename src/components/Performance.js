22fee63b86d321e2f0c983919e5f95b333de6cbf
whl <48339784+whl-gh@users.noreply.github.com>
2019-03-17 19:11:02 +0800

    完成运行情况统计页面

--- src/components/Performance.js
+++ src/components/Performance.js
@@ -1,9 +1,35 @@
 import React from "react";
 
-let Performance = (props)=>(
-    <div>
-        运行情况
-    </div>
-);
+let Performance = (props)=>{
+    return (
+        <div className="performance">
+            <table className="performance-tbl">
+                <caption className="text-left">信息统计(<small>最近12个月</small>)</caption>
+                <tbody>
+                    <tr>
+                        <td className="performance-title">累计需求数量</td>
+                        <td>3200</td>
+                        <td className="performance-title">已完成需求数量</td>
+                        <td>3158</td>
+                        <td className="performance-title">累计Bug数量</td>
+                        <td>6000</td>
+                        <td className="performance-title">已处理Bug数量</td>
+                        <td>4000</td>
+                    </tr>
+                    <tr>
+                        <td className="performance-title">本月需求数量</td>
+                        <td>300</td>
+                        <td className="performance-title">本月已完成需求数量</td>
+                        <td>179</td>
+                        <td className="performance-title">本月Bug数量</td>
+                        <td>1050</td>
+                        <td className="performance-title">本月已处理Bug数量</td>
+                        <td>792</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
 
 export default Performance;
---
+++ src/source/chartsData.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+      month: "1",
+      xunqiu: 70,
+      bug: 39
+    },
+    {
+      month: "2",
+      xunqiu: 69,
+      bug: 42
+    },
+    {
+      month: "3",
+      xunqiu: 95,
+      bug: 57
+    },
+    {
+      month: "4",
+      xunqiu: 145,
+      bug: 85
+    },
+    {
+      month: "5",
+      xunqiu: 184,
+      bug: 119
+    },
+    {
+      month: "6",
+      xunqiu: 215,
+      bug: 152
+    },
+    {
+      month: "7",
+      xunqiu: 252,
+      bug: 170
+    },
+    {
+      month: "8",
+      xunqiu: 265,
+      bug: 166
+    },
+    {
+      month: "9",
+      xunqiu: 233,
+      bug: 142
+    },
+    {
+      month: "10",
+      xunqiu: 18.3,
+      bug: 10.3
+    },
+    {
+      month: "11",
+      xunqiu: 139,
+      bug: 66
+    },
+    {
+      month: "12",
+      xunqiu: 96,
+      bug: 48
+    }
+];
+
+export default data;
